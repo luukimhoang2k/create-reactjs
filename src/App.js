6350f6cdc8706564d74fca03d4bdefd23f12687c
luukimhoang2k <luukimhoangdz2022@gmail.com>
2021-01-19 19:52:18 +0700

    first-app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={'https://scontent.fdad5-1.fna.fbcdn.net/v/t1.0-9/136684606_763780191217093_6837990110215288843_o.jpg?_nc_cat=111&ccb=2&_nc_sid=e3f864&_nc_ohc=DrYmaWO-9oYAX_Ai-wb&_nc_ht=scontent.fdad5-1.fna&oh=b554205063063d60f81bd9a35a937dbe&oe=602D72DC'} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/demo.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+class demo extends Component {
+  render() {
+    return (
+        <div>
+            <h1>
+                hello world!</h1> 
+        </div>
+  );
+}
+}
+export default demo;
